dcc6bc3b3682fead71708a861c712b0e5a82459b
Monique Lane <michellecolemanlane@gmail.com>
2022-05-13 15:33:30 -0400

    created popups
    created popups for gallery and contact page, removed templates and decided to go with popup option, added all classes for popups, current adjusting view for gallery popup.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,50 @@
-const pageTemplate = document.querySelectorAll('card');
+const popup = document.querySelector('.popup');
+const gallery = document.getElementById("gallery");
+const galleryButton = document.querySelector('.galleryButton')
+const contact = document.getElementById("contact");
+const contactButton = document.querySelector('.contactButton');
 
-console.log(pageTemplate)
+function closePopupWithEscape(event) {
+    if (event.key === "Escape") {
+        closeOpenedPopup();
+    }
+}
+
+function closeOpenedPopup() {
+    const openedPopups = document.querySelectorAll(".popup__container_active");
+    openedPopups.forEach((openedPopup) => closePopup(openedPopup));
+}
+
+
+function closePopup(popupElement) {
+    popupElement.classList.remove("popup__active");
+    popup.classList.remove("popup__container_active");
+    document.removeEventListener("keydown", closePopupWithEscape);
+}
+
+function openPopup(popupElement) {
+    popupElement.classList.add("popup__active");
+    popup.classList.add("popup__active");
+    document.addEventListener("keydown", closePopupWithEscape);
+}
+
+
+
+galleryButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    openPopup(gallery)
+});
+
+contactButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    console.log(contact)
+    openPopup(contact)
+});
+
+// const homeButton = document.querySelector(".homeButton");
+// homeButton.addEventListener("click", (evt) => {
+//     evt.preventDefault();
+//     console.log(evt.target)
+//     clearPage();
+//     addNewBody(homeClone)
+// });
