7dcccd08c816763cdae4294605ea6412214e48df
Monique Lane <michellecolemanlane@gmail.com>
2022-05-19 09:59:08 -0400

    Update scripts.js
    updated font weight to make question bold on click

--- JavaScript-Lab Resources/Accordion/scripts.js
+++ JavaScript-Lab Resources/Accordion/scripts.js
@@ -3,6 +3,7 @@ const sections = document.querySelectorAll('.question-section');
 sections.forEach((section) => {
     section.addEventListener("click", function(evt) {
         evt.preventDefault();
+        section.style.fontWeight = 900;
         section.nextElementSibling.classList.toggle('hidden');
     })
 })
