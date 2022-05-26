acb0ba60ba2a0a2defeb376113855a08b462b5f0
Monique Lane <michellecolemanlane@gmail.com>
2022-05-26 00:19:14 -0400

    node modules added
    node modules added

---
+++ glitter/src/ProfileView.js
@@ -0,0 +1,16 @@
+export default class ProfileView extends React.Component {
+    constructor(props) {
+        this.state = [{
+            user: "Random User",
+            photo: "img.src",
+            pronouns: "she/they",
+            location: "Orlando, FL USA",
+            Birthday: "July 26, 91",
+        }, ];
+        super(props)
+    }
+    render() {
+        return 'testing'
+    }
+
+}
