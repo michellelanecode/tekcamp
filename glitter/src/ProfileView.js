5ad0cf7a73aff4c9de2e3a04fdc09b1e509ee53e
Monique Lane <michellecolemanlane@gmail.com>
2022-05-26 21:33:59 -0400

    commit 2
    added load user info method, using axios returns user information to use for user profile

--- glitter/src/ProfileView.js
+++ glitter/src/ProfileView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 export default class ProfileView extends React.Component {
     constructor() {
@@ -9,17 +10,31 @@ export default class ProfileView extends React.Component {
             pronouns: "she/they",
             image: '../images/2289_SkVNQSBGQU1PIDEwMjgtMTE0-removebg-preview.png'
         };
+
+        this.loadUserInfo();
+    }
+
+
+    loadUserInfo() {
+        const apiKey = '628fa168f8a8b8aecbe96686';
+        axios.get('https://dummyapi.io/data/v1/user?page=1&limit=10', {
+            headers: {
+                'app-id': apiKey,
+            }
+        })
     }
 
     render() {
         return ( <
             div className = "five wide column" >
             <
+            div className = 'profile-image-container' > <
             img className = "profile-image"
             src = { this.state.image }
             alt = "this user pic isn't real" /
             >
             <
+            /div> <
             div className = "profile-information" >
             <
             p className = "profile-username" > { this.state.location } < /p> <
