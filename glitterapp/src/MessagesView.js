aab350e800cc0115cddf7c9a5aecf667b0569932
Monique Lane <michellecolemanlane@gmail.com>
2022-05-28 20:17:07 -0500

    messages update
    Able to retrieve messages from dummy api

--- glitterapp/src/MessagesView.js
+++ glitterapp/src/MessagesView.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 export default class MessagesView extends React.Component {
-        constructor() {
-            super();
-            this.state = {};
-        }
+  constructor() {
+    super();
+    this.state = { messages: {} };
+  }
 
-        render() {
-            return ( < div className = 'five wide column' > testing Message View < /div>)
-            }
-        }
+  updateMessages(messages) {
+    this.setState((state) => {
+      state.messages = messages;
+    });
+  }
+
+  componentDidMount() {
+    const apiKey = "628fa168f8a8b8aecbe96686";
+    axios
+      .get("https://dummyapi.io/data/v1/post", {
+        headers: {
+          "app-id": apiKey,
+        },
+      })
+      .then((res) => {
+        this.updateMessages(res.data);
+        console.log("these are messages:", res.data);
+      });
+  }
+
+  render() {
+    return <div className="five wide column"> testing Message View </div>;
+  }
+}
--- glitterapp/src/User.js
+++ glitterapp/src/User.js
@@ -3,16 +3,22 @@ import React from "react";
 export default class User extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
   }
 
   render() {
     return (
       <div className="profile-image-container">
-        <img className="profile-image" src="" alt="this user pic isn't real" />
+        <img
+          className="profile-image"
+          src={this.props.userInfo.user.picture}
+          alt="this user pic isn't real"
+        />
 
         <div className="profile-information">
-          <p className="profile-username">Name and Gender</p>
+          <p className="profile-username">
+            {this.props.userInfo.user.firstName}{" "}
+            {this.props.userInfo.user.lastName} and Gender
+          </p>
           <p className="profile-location">Location</p>
         </div>
       </div>
