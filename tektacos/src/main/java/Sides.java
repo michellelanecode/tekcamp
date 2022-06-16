5e320a512d9950d42c6019c12fe15826b5153bd6
Monique Lane <michellecolemanlane@gmail.com>
2022-06-16 11:53:09 -0400

    Update Sides.java
    updated sides to include enum and new choosing method

--- tektacos/src/main/java/Sides.java
+++ tektacos/src/main/java/Sides.java
@@ -5,11 +5,33 @@ public class Sides  {
 
     String side = "SIDE: ";
 
+    public enum sideChoices {
+        CHIPSANDQUESO,
+        CHIPSANDSALSA,
+        FRIES
+    }
+
     public String getSide() {
         Scanner sc = new Scanner(System.in);
            System.out.println("What kind of side would you like?");
-           this.side = sc.nextLine();
-         return  this.returnSide();
+           String side = sc.nextLine();
+         return  this.getSideEnum(side);
+    }
+
+
+    public String getSideEnum(String side){
+        switch (side){
+            case "cq":
+                return this.side = this.side + sideChoices.CHIPSANDQUESO;
+
+            case "cs":
+                return this.side = this.side + sideChoices.CHIPSANDSALSA;
+
+            case "f":
+                return this.side = this.side + sideChoices.FRIES;
+
+        }
+        return  "NO CHOICE SELECTED";
     }
     public String returnSide(){
         return this.side;
