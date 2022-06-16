5205cc0bffb75e5cfd5dd31352c233901dc203d2
Monique Lane <michellecolemanlane@gmail.com>
2022-06-16 11:45:14 -0400

    update
    updated drink class to add enum drink choices and getDrinkEnum method

--- tektacos/src/main/java/Drinks.java
+++ tektacos/src/main/java/Drinks.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 
 public class Drinks {
-    String drinkChoice;
+    String drinkChoice = "DRINK: ";
 
-    public String getDrinkChoice() {
+    public enum DrinkChoices {
+        SODA,
+        TEA,
+        WATER
+    }
+
+    public void getDrinkChoice() {
         Scanner sc = new Scanner(System.in);
         System.out.println("What kind of drink would you like?");
-        drinkChoice = sc.nextLine();
-        return this.returnDrinkChoice();
+        String drink = sc.nextLine();
+         this.getDrinkEnum(drink);
+    }
+
+    public void getDrinkEnum(String drink){
+        switch (drinkChoice){
+            case "soda":
+                this.drinkChoice = this.drinkChoice + DrinkChoices.SODA;
+                break;
+            case "tea":
+                this.drinkChoice = this.drinkChoice + DrinkChoices.TEA;
+                break;
+            case "water":
+                this.drinkChoice = this.drinkChoice + DrinkChoices.WATER;
+                break;
+        }
     }
     public String returnDrinkChoice(){
         return this.drinkChoice;
