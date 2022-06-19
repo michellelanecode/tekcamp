f45e2beb83f70996b39f5a3a077441218a25e32b
Monique Lane <michellecolemanlane@gmail.com>
2022-06-19 08:50:06 -0400

    update
    updated menu

--- tektacos/src/main/java/com/tektacos/main/Menu.java
+++ tektacos/src/main/java/com/tektacos/main/Menu.java
@@ -1,12 +1,12 @@
 package com.tektacos.main;
 
 public class Menu {
-    String comboMenu = "**Note: Combo includes Taco Order_Types.Entree Choice plus Customer_Inputs.Toppings, Side and Drink**";
-    String proteinMenu = "Customer_Inputs.Proteins: beef(b), chicken(c), steak(s), pork(p), black beans(bb) ";
+    String comboMenu = "**Note: Combo includes Taco Entree Choice plus Toppings, Side and Drink**";
+    String proteinMenu = "Proteins: beef(b), chicken(c), steak(s), pork(p), black beans(bb) ";
     String toppingsMenu = "Lettuce(l), Tomato(t), Cheese(cs), Salsa(s), Cilantro(ct), Jalapeno(j), Onion(o)";
-    String entreeMenu = "Order_Types.Entree menu: Entree_Types.Basic - Customer_Inputs.Tortilla (flour or corn) Protein and 4 toppings %n" +
-            "Entree_Types.Deluxe - Entree_Types.Basic plus lettuce, tomato, onion, cheese and 2 additional toppings %n" +
-            "Entree_Types.Veggie - Customer_Inputs.Tortilla (flour or corn) Black beans and 4 toppings";
+    String entreeMenu = "Entree menu: Basic - Tortilla (flour or corn) Protein and 4 toppings %n" +
+            "Deluxe - Basic plus lettuce, tomato, onion, cheese and 2 additional toppings %n" +
+            "Veggie - Tortilla (flour or corn) Black beans and 4 toppings";
 
     String line = "____________________________";
 
