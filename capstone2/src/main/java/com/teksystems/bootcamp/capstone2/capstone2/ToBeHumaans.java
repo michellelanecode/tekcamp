f5efde9a0a14b337e41962e85b5e83dd4934e1a3
Monique Lane <michellecolemanlane@gmail.com>
2022-06-23 17:16:46 -0400

    Created 3 Scenes
    set up forest 1 scene, game intro scene, and character scene

--- capstone2/src/main/java/com/teksystems/bootcamp/capstone2/capstone2/ToBeHumaans.java
+++ capstone2/src/main/java/com/teksystems/bootcamp/capstone2/capstone2/ToBeHumaans.java
@@ -2,16 +2,19 @@ package com.teksystems.bootcamp.capstone2.capstone2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ToBeHumaans extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ToBeHumaans.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
         stage.setTitle("To Be Humaan");
         stage.setScene(scene);
         stage.show();
--- capstone2/src/main/java/com/teksystems/bootcamp/capstone2/capstone2/ToBeHumaansController.java
+++ capstone2/src/main/java/com/teksystems/bootcamp/capstone2/capstone2/ToBeHumaansController.java
@@ -1,14 +1,38 @@
 package com.teksystems.bootcamp.capstone2.capstone2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ToBeHumaansController {
     @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        System.out.println("This is working");
+
+
+@FXML
+    public void switchToCharacterScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("characterscene.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+@FXML
+    public void switchToForestOneScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("forest1.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
